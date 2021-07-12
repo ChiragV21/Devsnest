8011908fe5c966273da36ea3202bc287a1cffe7a
Chirag Varshney <55586574+ChiragV21@users.noreply.github.com>
2021-07-13 00:17:01 +0530

    Add files via upload

---
+++ Day-18/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chess from './Chess.jsx';
+import './style.css';
+
+ReactDOM.render(
+    <>
+       <Chess/>
+    </>,
+    document.getElementById("root")
+);
