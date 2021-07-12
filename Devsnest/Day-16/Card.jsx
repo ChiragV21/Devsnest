140bc0b6e54f6f35e366893653002a6a224d4de3
Chirag Varshney <55586574+ChiragV21@users.noreply.github.com>
2021-07-12 15:45:33 +0530

    Add files via upload

---
+++ Devsnest/Day-16/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = () => {
+
+    return (
+        <>
+            <div className="card">
+                <img src="https://tsh.io/wp-content/uploads/2019/12/react-meme1_.png" />
+                <div class="container">
+                    <h2><b>React Card</b></h2>
+                    <p>your main THA is to make meme cards</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Card;
---
+++ Devsnest/Day-16/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card.jsx';
+import './style.css';
+
+ReactDOM.render(
+    <Card />, document.getElementById("root")
+);
