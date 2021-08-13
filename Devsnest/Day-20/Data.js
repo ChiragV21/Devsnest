6bb3325ab8a72125f4efdf038968980c91be11d8
Chirag Varshney <55586574+ChiragV21@users.noreply.github.com>
2021-08-13 23:10:04 +0530

    Add files via upload

---
+++ Devsnest/Day-20/Data.js
@@ -0,0 +1,39 @@
+const Data=[
+    {
+        id:0,
+        food:"Pizza",
+        calory:"56",
+    },
+    {
+        id:1,
+        food:"Burger",
+        calory:"69",
+    },
+    {
+        id:2,
+        food:"Coke",
+        calory:"500",
+    },
+    {
+        id:3,
+        food:"Browne",
+        calory:"180",
+    },
+    {
+        id:4,
+        food:"Fried Rice",
+        calory:"90",
+    },
+    {
+        id:5,
+        food:"Lassania",
+        calory:"200",
+    },
+    {
+        id:7,
+        food:"Pani Puri",
+        calory:"10",
+    },
+];
+
+export default Data;
